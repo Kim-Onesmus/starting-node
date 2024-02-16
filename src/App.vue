5b4dc05e1f57c3a6241b7d33faec9452f3559268
Kim-K <kim-k@localhost.localdomain>
2024-02-16 15:28:43 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+<body>
+  <h1>Welcome to vueJS</h1>
+</body>
+</template>
+
+<style scoped>
+body {
+  background-color: aliceblue;
+}
+h1 {
+  color: red;
+  background-color: aliceblue;
+}
+</style>
